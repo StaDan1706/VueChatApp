68a6e8806ed92eee75a76b7ab06b4db6e681f129
Damon <stanislaw.d@icloud.com>
2023-05-30 12:28:16 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,16 +3,17 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from "firebase/database";
 
 
-console.log("X")
-console.log("2" + process.env.VITE_VUE_APP_FIREBASE_API_KEY)
-console.log("3" + process.VITE_VUE_APP_FIREBASE_API_KEY )
-console.log("4" + import.meta.env.VITE_VUE_APP_FIREBASE_API_KEY)
+
+console.log("Api key" + import.meta.env.VITE_VUE_APP_FIREBASE_API_KEY)
+console.log("Project id" + import.meta.env.VITE_VUE_APP_FIREBASE_PROJECT_ID)
+
+
 const firebaseConfig = {
-    apiKey: process.env.VITE_VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "firebaseapp.com",
+    apiKey: import.meta.env.VITE_VUE_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "firebaseapp.com",
     databaseURL:  "https://chatapp-c02f9-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "chatapp-c02f9",
-    storageBucket: process.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "appspot.com",
+    storageBucket: import.meta.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "appspot.com",
     messagingSenderId: "586992265067",
     appId: "1:586992265067:web:94c5c75ba6fa83825f4e2e",
     measurementId: "G-6ZNHHHS444"
