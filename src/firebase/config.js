3344b7d3ed7898ebdcbc4540fd4bcbe30f6b195d
Damon <stanislaw.d@icloud.com>
2023-05-30 10:53:23 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,11 +5,11 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: env:import VITE_VUE_APP_FIREBASE_API_KEY,
-    authDomain: env:import VITE_VUE_APP_FIREBASE_PROJECT_ID + "firebaseapp.com",
+    apiKey: import.meta.env.VITE_VUE_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "firebaseapp.com",
     databaseURL:  "https://chatapp-c02f9-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "chatapp-c02f9",
-    storageBucket: env:import VITE_VUE_APP_FIREBASE_PROJECT_ID + "appspot.com",
+    storageBucket: import.meta.env.VITE_VUE_APP_FIREBASE_PROJECT_ID + "appspot.com",
     messagingSenderId: "586992265067",
     appId: "1:586992265067:web:94c5c75ba6fa83825f4e2e",
     measurementId: "G-6ZNHHHS444"
